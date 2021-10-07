578ca79afb7ea97c10270f9c0c5f1e9843b227d8
ash <ash@topdog.ru.net>
2021-10-07 15:45:18 +0300

    added server logging3

--- logs/system_logger.py
+++ logs/system_logger.py
@@ -5,18 +5,19 @@ import traceback
 import logs.client_conf_log
 import logs.server_conf_log
 
-if sys.argv[0].find('server') >= 0:
-    logger = logging.getLogger('app.server')
-else:
-    logger = logging.getLogger('app.client')
-
 
 class SystemLogger:
+    def __init__(self):
+        if sys.argv[0].find('server') >= 0:
+            self.logger = logging.getLogger('app.server')
+        else:
+            self.logger = logging.getLogger('app.client')
+
     def __call__(self, func):
         def wrap(*args, **kwargs):
             result = func(*args, **kwargs)
-            logger.debug(f'Вызвана функция с именем: {func.__name__}. Параметры функции: {args}, {kwargs}. '
+            self.logger.debug(f'Вызвана функция с именем: {func.__name__}, аргументы функции: {args}, {kwargs}. '
                          f'Вызов из модуля {func.__module__}.{traceback.format_stack()[0].strip().split()[-1]}.'
-                         f'Вызов из функции {inspect.stack()[1][3]}')
+                         f'Вызов из {inspect.stack()[1][3]}')
             return result
         return wrap
